d0d20b17068d8512cbdf7b575857756461ced7ff
spences10 <spences10apps@gmail.com>
2020-01-02 11:31:40 +0000

    configure useAnalytics

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,61 +1,94 @@
+import { graphql, Link, StaticQuery } from 'gatsby'
 import React from 'react'
-import { Link, graphql, StaticQuery } from 'gatsby'
+import { useAnalytics } from '../../contexts/event-tracking'
 import SearchBox from '../SearchBox'
 
-const NavBar = ({ toggleNavbar, isActive }) => (
-  <StaticQuery
-    query={graphql`
-            query SearchIndexQuery {
-                siteSearchIndex {
-                    index
-                }
-            }
-        `}
-    render={data => (
-      <nav className='navbar is-fixed-top' aria-label='main navigation'>
-        <div className='navbar-brand'>
-          <Link to='/' className='navbar-item'>
-            <strong>Gatsby Starter Business</strong>
-          </Link>
-          <button
-            className={`button navbar-burger ${isActive ? 'is-active' : ''}`}
-            data-target='navMenu'
-            onClick={toggleNavbar}
+const NavBar = ({ toggleNavbar, isActive }) => {
+  const fa = useAnalytics()
+  return (
+    <StaticQuery
+      query={graphql`
+        query SearchIndexQuery {
+          siteSearchIndex {
+            index
+          }
+        }
+      `}
+      render={data => (
+        <nav
+          className="navbar is-fixed-top"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <strong>Gatsby Starter Business</strong>
+            </Link>
+            <button
+              className={`button navbar-burger ${
+                isActive ? 'is-active' : ''
+              }`}
+              data-target="navMenu"
+              onClick={toggleNavbar}
+            >
+              <span />
+              <span />
+              <span />
+            </button>
+          </div>
+          <div
+            className={`navbar-menu ${isActive ? 'is-active' : ''}`}
+            id="navMenu"
           >
-            <span />
-            <span />
-            <span />
-          </button>
-        </div>
-        <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id='navMenu'>
-
-          <div className='navbar-end'>
-            <SearchBox searchIndex={data.siteSearchIndex.index} />
-            <Link className='navbar-item' to='/about'>
-                            About
-            </Link>
-            <Link className='navbar-item' to='/pricing'>
-                            Pricing
-            </Link>
-            <Link className='navbar-item' to='/blog'>
-                            Blog
-            </Link>
-            <div className='navbar-item'>
-              <div className='field is-grouped'>
-                <p className='control'>
-                  <Link
-                    className='button is-primary is-outlined'
-                    to='/contact'>
-                            Contact Us
-                  </Link>
-                </p>
+            <div className="navbar-end">
+              <SearchBox searchIndex={data.siteSearchIndex.index} />
+              <Link
+                onClick={() => {
+                  fa('CYYZD97C')
+                }}
+                className="navbar-item"
+                to="/about"
+              >
+                About
+              </Link>
+              <Link
+                onClick={() => {
+                  fa('YNK3JR3X')
+                }}
+                className="navbar-item"
+                to="/pricing"
+              >
+                Pricing
+              </Link>
+              <Link
+                onClick={() => {
+                  fa('VO7LTLOY')
+                }}
+                className="navbar-item"
+                to="/blog"
+              >
+                Blog
+              </Link>
+              <div className="navbar-item">
+                <div className="field is-grouped">
+                  <p className="control">
+                    <Link
+                      onClick={() => {
+                        fa('TDY5DIIA')
+                      }}
+                      className="button is-primary is-outlined"
+                      to="/contact"
+                    >
+                      Contact Us
+                    </Link>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    )}
-  />
-)
+        </nav>
+      )}
+    />
+  )
+}
 
 export default NavBar
